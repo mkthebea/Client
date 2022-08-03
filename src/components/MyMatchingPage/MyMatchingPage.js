34d23812271f51e9a6099af94016dcf79bae1bfa
mkthebea <5596molly@naver.com>
2022-08-03 23:28:51 +0900

    new matching page

--- src/components/MyMatchingPage/MyMatchingPage.js
+++ src/components/MyMatchingPage/MyMatchingPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./MyMatchingPage.module.css";
 
 function MyMatchingPage() {
-  return <div>MyMatchingPage 내 매칭 페이지</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.form_container}>
+        <div className={styles.text}>My MatChing</div>
+      </div>
+    </div>
+  );
 }
 
 export default MyMatchingPage;
--- src/components/NewMatchingPage/NewMatchingPage.js
+++ src/components/NewMatchingPage/NewMatchingPage.js
@@ -24,39 +24,45 @@ function NewMatchingPage() {
           layout="horizontal"
           onFinish={onFinish}
         >
-          <Form.Item label="식당 이름" name={"name"}>
+          <Form.Item label="식당 이름" name={"name"} rules={[{ required: true, message: "식당 이름을 입력하세요" }]}>
             <Input />
           </Form.Item>
-          <Form.Item label="성별" name={["tags", "gender"]}>
+          <Form.Item label="성별" name={["tags", "gender"]} rules={[{ required: true, message: "성별을 선택하세요" }]}>
             <Select>
               <Select.Option value="woman">여성</Select.Option>
               <Select.Option value="man">남성</Select.Option>
               <Select.Option value="both">성별 무관</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label="학과" name={["tags", "major"]}>
+          <Form.Item label="학과" name={["tags", "major"]} rules={[{ required: true, message: "학과를 선택하세요" }]}>
             <Select>
               <Select.Option value="demo">Demo</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label="만남 모드" name={["tags", "mode"]}>
+          <Form.Item label="만남 모드" name={["tags", "mode"]} rules={[{ required: true, message: "만남 모드를 선택하세요" }]}>
             <Select>
               <Select.Option value="eat">밥만 먹어요</Select.Option>
               <Select.Option value="friend">우리 친해져요</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label="날짜" name={"date"}>
+          <Form.Item label="날짜" name={"date"} rules={[{ required: true, message: "날짜를 선택하세요" }]}>
             <DatePicker /> &nbsp;&nbsp;
             <TimePicker.RangePicker />
           </Form.Item>
-          <Form.Item label="최대 인원" name={"max"}>
+          <Form.Item label="최대 인원" name={"max"} rules={[{ required: true, message: "최대 인원을 입력하세요" }]}>
             <InputNumber min={2} max={10} defaultValue={3} />
           </Form.Item>
           <Form.Item label="한줄 소개" name={"description"}>
             <TextArea rows={3} placeholder="maxLength is 30" maxLength={30} />
           </Form.Item>
 
-          <Form.Item className={styles.button_container}>
+          <Form.Item
+            className={styles.button_container}
+            wrapperCol={{
+              offset: 11,
+              // span: 12,
+            }}
+          >
             <Button type="primary" htmlType="submit" className={styles.button}>
               등록하기
             </Button>
