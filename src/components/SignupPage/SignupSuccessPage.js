7020b353185040e46aec74c955258afb1abb92da
mkthebea <5596molly@naver.com>
2022-09-11 19:32:23 +0900

    signup success page: pass credentials

--- src/components/SignupPage/SignupSuccessPage.js
+++ src/components/SignupPage/SignupSuccessPage.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { Result, Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { Result, Button, message } from "antd";
+import { useLocation } from "react-router-dom";
+import qs from "qs";
 import styles from "./SignupPage.module.css";
+import axios from "axios";
 
 function SignupSuccessPage() {
+  const location = useLocation();
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const token = query.token;
+  const uid64 = query.uid64;
+  const signupRequest = async () => {
+    const response = await axios.get(`/api/account/activate/${uid64}/${token}`);
+    // console.log(response);
+    // if (!response.data.success) message.error("알 수 없는 오류 발생");
+  };
+  useEffect(() => {
+    signupRequest();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.form_container}>
